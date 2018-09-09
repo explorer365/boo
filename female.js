cc04a2ea768582a34864f10f6f4c2e0865564964
Cross <danikay356@gmail.com>
2018-09-09 19:56:29 +0000

    question changes

--- female.js
+++ female.js
@@ -1,18 +1,18 @@
 questions = [
     {
-        "question": "I want a buff man who is unable to provide for my basic needs",
+        "question": "I want a 6 feet plus man who is unable to provide for my basic needs",
         "variable": ["Physique","Finance"]
     }, {
-        "question": "I want a brilliant man, it doesn’t matter whether he knows the g in God",
+        "question": "I want a man who has solid character. He doesn't have to believe in God",
         "variable": ["Character","Spiritual"]
     }, {
         "question": "I want a man who is a go-getter, so what if he’s a 1-minute man in the other room",
         "variable": ["Drive","Sex"]
     }, {
-        "question": "I want a good looking man. God is not really important",
+        "question": "I just want a really good looking man. God is not really important",
         "variable": ["Physique","Spiritual"]
     }, {
-        "question": "I want a tiger on the sheets, take me to the highest heavens. Well about his character, no one is perfect",
+        "question": "I want a tiger on the sheets, take me to the highest heavens. I don't care about his character",
         "variable": ["Sex","Character"]
     }, {
         "question": "I want ambition personified, hard worker. God will understand if he’s too busy",
@@ -23,7 +23,8 @@ questions = [
     }, {
         "question": "I want a fit man. Yes he’s not really fit down there but I’m fine with it",
         "variable": ["Physique","Sex"]
-    }, {   "question": "I want a very prayerful and godly fellow. Money isn’t that important",
+    }, {   
+        "question": "I want a very prayerful and godly fellow. Money isn’t that important",
         "variable": ["Spiritual","Finance"]
     }, {
         "question": "I want a man that’d rather starve than steal but he’s not driven to do great stuff ",
