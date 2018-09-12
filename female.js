8f2d724b1f063a05f52544ee5b630fd42414feff
Cross <danikay356@gmail.com>
2018-09-12 01:03:16 +0000

    multiquestions

--- female.js
+++ female.js
@@ -1,33 +1,73 @@
+var insertA = ["I want a 6ft+ man who can’t provide food my basic needs", "A very well built man is my ideal man. It doesn’t matter if he’s broke", "A broke man is not as broke if you take a look at his biceps and triceps closely", "Since I make my own money, his doesn’t matter so long as he has the muscles"];
+
+var insertB = ["I want a brilliant man, it doesn’t matter whether he knows the g in God", "Prayer is overrated. Have sense and a solid character and we’re good", "Vigils? What’s that? Please spare me! If he has a good character, we’re good to go", "I want a solid character. His spiritual life has no bearing on me"];
+
+var insertC = ["I want a man who is a go-getter, so what if he’s a 1-minute man in the other room", "He’s really in love with what he does, that’s the only orgasm that I require", "This man is driven to achieve his goals !!! Is sex food? We’d be fine without it or a little", "Sex is overrated. I’d rather a man who is very ambitious in other things than on the bed"];
+
+var insertD = ["I want a really good looking man. Whether he knows God or not  is not as important", "I want him at least 6 feet, tall and handsome. He doesn't need to pray. I’d do the praying for both of us", "Give me beards I can play with, pacs that I can run my fingers through. His ability to pray is inconsequential", "He has to be really attractive.  I really don’t care about his spiritual life."];
+
+var insertE = ["I want a tiger on the sheets, take me to the highest heavens. Well, about his character, no one is perfect.", "Don’t stop baby. Don’t stop. I know you lied yesterday and your character is constantly in question but just rock me all night long", "Kissing for breakfast, cuddling for lunch and orgasms for dinner. Some lies and other escapades are acceptable", "Kamasutra is our bible. all problems can be solved including his morally bankrupt character after a hot steamy session"];
+
+var insertF = ["I want ambition personified, hard worker. God will understand if he’s too busy", "He is a finisher and highly productive in all areas but spiritual stuff", "If  he’s very driven to achieve goals, I’m not bothered if he prays only once a week or month", "If he puts his mind at anything, consider it done. Thing is he hasn’t exactly put his mind on anything spiritual but I’m cool with that"];
+
+var insertG = ["If he has money and a good amount of it, his character doesn’t really matter", "Money solves all things. I think his bad character would improve with time", "Does he have money for my regular vacations and an exquisite wardrobe? We’d deal with the rest later", "Pepper them gang all day, everyday. Who has character helped?"];
+
+var insertH = ["I want a fit man. Yes he’s not gifted down there but I’m fine with it.", "Our sex life is horrible but he’s physically attractive", "One-minute man is manageable. But have you seen how mind-blowing he looks? I love!!", "His muscles  are good enough for me even if he doesn’t know how to use the man down there."];
+
+var insertI = ["I want a very prayerful and godly man.I prefer that to his having money", "He can’t make 10 sentences without mentioning something about the supernatural. That matters more to me than all the money in the world", "He’s very religious and has just enough money", "If there’s any iota of spirituality in it, he’s there and doesn’t have enough money. I have my money"];
+
+var insertJ = ["I want a man who has an awesome character even though she is not driven to achieve anything.", "He’s a good person deep inside but has absolutely no motivation to do anything", "Honesty, loyalty, and responsibility are a few of his good traits but he’s clueless about what she wants", "A good name is better than everything else including drive and ambition"];
+
+var rand1 = insertA[Math.floor(Math.random() * insertA.length)];
+
+var rand2 = insertB[Math.floor(Math.random() * insertB.length)];
+
+var rand3 = insertC[Math.floor(Math.random() * insertC.length)];
+
+var rand4 = insertD[Math.floor(Math.random() * insertD.length)];
+
+var rand5 = insertE[Math.floor(Math.random() * insertE.length)];
+
+var rand6 = insertF[Math.floor(Math.random() * insertF.length)];
+
+var rand7 = insertG[Math.floor(Math.random() * insertG.length)];
+
+var rand8 = insertH[Math.floor(Math.random() * insertH.length)];
+
+var rand9 = insertI[Math.floor(Math.random() * insertI.length)];
+
+var rand10 = insertJ[Math.floor(Math.random() * insertJ.length)];
+
 questions = [
     {
-        "question": "I want a 6 feet plus man who is unable to provide for my basic needs",
+        "question": rand1,
         "variable": ["Physique","Finance"]
     }, {
-        "question": "I want a man who has solid character. He doesn't have to believe in God",
+        "question": rand2,
         "variable": ["Character","Spiritual"]
     }, {
-        "question": "I want a man who is a go-getter, so what if he’s a 1-minute man in the other room",
+        "question": rand3,
         "variable": ["Drive","Sex"]
     }, {
-        "question": "I just want a really good looking man. God is not really important",
+        "question": rand4,
         "variable": ["Physique","Spiritual"]
     }, {
-        "question": "I want a tiger on the sheets, take me to the highest heavens. I don't care about his character",
+        "question": rand5,
         "variable": ["Sex","Character"]
     }, {
-        "question": "I want ambition personified, hard worker. God will understand if he’s too busy",
+        "question": rand6,
         "variable": ["Drive","Spiritual"]
     }, {
-        "question": "Let him just have money. How he gets it doesn’t really matter. Does it?",
+        "question": rand7,
         "variable": ["Finance","Character"]
     }, {
-        "question": "I want a fit man. Yes he’s not really fit down there but I’m fine with it",
+        "question": rand8,
         "variable": ["Physique","Sex"]
     }, {   
-        "question": "I want a very prayerful and godly fellow. Money isn’t that important",
+        "question": rand9,
         "variable": ["Spiritual","Finance"]
     }, {
-        "question": "I want a man that’d rather starve than steal but he’s not driven to do great stuff ",
+        "question": rand10,
         "variable": ["Character","Drive"]
     },
 
--- male.js
+++ male.js
@@ -1,62 +1,83 @@
- 
-var insertA = ['Willy the Goblin','Big Daddy','Father Christmas', 'fello', 'juicy'];
-var insertB = ['the soup kitchen','Disneyland','the White House'];
-var insertC = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+// To be answered by Men
 
-var rand = insertA[Math.floor(Math.random() * insertA.length)];
+var insertA = ["I want a woman that has the body of a goddess. So what if she’s broke?", "Is she gifted in the right places? That’s what matters. I’d handle all the monetary needs", "If she has the body of Kim, I will provide the money Kanye has", "Her body is fire. Her pocket is empty. Give me!!!!"];
 
-/* 
-var sup = 
-randomize.addEventListener('click', result);
+var insertB = ["I want a woman who is intelligent and is well trained. It doesn’t matter if she’s not spiritual", "There’s no topic she can’t talk about. Very versatile about everything but spiritual matters", "She is smart and intelligent but all the things religious people do are not her thing", "She’s a good woman and very smart. The importance of religion is often over emphasized"];
 
-//function result() {
-    var rand = insertA[Math.floor(Math.random() * insertA.length)];
-    //return rand;
-//}
-*/
-//var x = result();
+var insertC = ["I want a highly ambitious woman. Who cares if we have sex once a month or quarter?", "An intelligent woman who can hold it down is better than a woman who can last forever in bed", "She’s highly ambitious and it’s fine if it means very little action in the other room", "She is so hardworking and applies herself in everything. Everything but sex."];
 
-//I want a woman that has the body of a goddess. So what if she’s broke
+var insertD = ["I want a woman with awesome curves and a pretty face that isn’t spiritual. I can’t be bothered.", "Her looks and cat walk blows me away. She doesn’t have to be spiritual. Who has spiritual helped?", "I will do all the praying and other religious duties. All I want is  for her to have a banging body", "She doesn’t need to believe in anything spiritual. Her attractiveness is more important to me."];
+
+var insertE = ["I want a woman of solid character. It doesn't matter if the sex is boring and happens once a month.", "Does she have an awesome character? Then yes. Everything else including sex doesn’t really matter", "Her character is much more important than anything else to me. Her sexual ability is not so important", "Her character is worth emulating. A role model to role models. Her cluelessness when it comes to sex is unrivaled"];
+
+var insertF = ["I want a highly ambitious woman even if she may earn more than I so long as she doesn’t bore me with spiritual stuff", "She’s definitely on her way to take over the world. She’s not a religious person and that’s fine with me", "Tenacious, Hardworking and persistence is who she is. Prayers and all those religious stuff don’t go down well with her though", "She’s disciplined and has a work-ethic that’s enviable. She’s not a religious person and i don’t mind"];
+
+var insertG = ["I want a woman who makes her money. She could have a bad character but that’s fine", "She has all the money in the world but she is character bankrupt.", "Money is not a problem for her. Her character is a problem though but that’s fine ", "So long as there’s a lot of money in the bank, her character doesn’t matter one bit"];
+
+var insertH = ["I want a woman who can blow my brains out, not literally. So what if she’s not there physically", "I’m not interested in a fine body especially if she’s very good in bed", "We’d make love on the couch, in the bath, any and everywhere. Everything else including her physique is unimportant", "She might be the prettiest in the world but if she can’t match me in the bedroom, I’m definitely not interested."];
+
+var insertI = ["I want a woman who would do all the spiritual stuff on my behalf. She can depend solely on me for money", "It’s okay if she’s broke so long as she’s very spiritual", "She’s very religious but has very little money and that's fine. I’m supposed to provide anyways", "She’s as broke as can be but very religious. I’m happy with that to be honest"];
+
+var insertJ = ["I want a woman who has an awesome character even though she is not driven to achieve anything.", "She’s a good person deep inside but has absolutely no motivation to do anything", "Honesty, loyalty and  compassion are a few of her good traits but she’s clueless about what she wants", "She’s kind, generous, reliable and loving but is not motivated to achieve anything in particular"];
+
+var rand1 = insertA[Math.floor(Math.random() * insertA.length)];
+
+var rand2 = insertB[Math.floor(Math.random() * insertB.length)];
+
+var rand3 = insertC[Math.floor(Math.random() * insertC.length)];
+var rand4 = insertD[Math.floor(Math.random() * insertD.length)];
+
+var rand5 = insertE[Math.floor(Math.random() * insertE.length)];
+
+var rand6 = insertF[Math.floor(Math.random() * insertF.length)];
+
+var rand7 = insertG[Math.floor(Math.random() * insertG.length)];
+
+var rand8 = insertH[Math.floor(Math.random() * insertH.length)];
+
+var rand9 = insertI[Math.floor(Math.random() * insertI.length)];
+
+var rand10 = insertJ[Math.floor(Math.random() * insertJ.length)];
 
 questions = [
     {
-        "question": rand,
+        "question": rand1,
         "variable": ["Physique","Finance"]
     },
     {
-        "question": "I want a woman who is very intelligent and of good character. It doesn’t matter if she's agnostic",
+        "question": rand2,
         "variable": ["Character","Spiritual"]
     }, 
     {
-        "question": "I want a highly ambitious woman. It doesn't matter if  cares if we have sex once a month or quarter?",
+        "question": rand3,
         "variable": ["Drive","Sex"]
     }, 
     {
-        "question": "I want a woman with awesome curves and a pretty face. I'm not so bothered about all the spiritual stuff.",
+        "question": rand4,
         "variable": ["Physique","Spiritual"]
     }, 
     {
-        "question": "I want a woman of solid character.It doesn't matter if the sex is boring and unexciting",
+        "question": rand5,
         "variable": ["Character","Sex"]
     }, 
     {
-        "question": "I want a highly ambitious woman. I don't like over religious people",
+        "question": rand6,
         "variable": ["Drive","Spiritual"]
     }, 
     {
-        "question": "I want a woman who makes a ton of money. Her character isn't important to me",
+        "question": rand7,
         "variable": ["Finance","Character"]
     }, 
     {
-        "question": "I want a woman who can blow my brains out, not literally. So what if she’s doesn't have the curves",
+        "question": rand8,
         "variable": ["Sex","Physique"]
     },
     {
-        "question": "I want a woman who would do all the spiritual stuff on my behalf. She can depend solely on me for money",
+        "question": rand9,
         "variable": ["Spiritual","Finance"]
     },
     {
-        "question": "I want a woman who has an awesome character even though she is not driven to achieve anything.",
+        "question": rand10,
         "variable": ["Character","Drive"]
     },
 ];
